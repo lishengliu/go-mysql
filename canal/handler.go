9d434215f27947177cf33293ecc3b300ce2deb37
Eric Liu <lisheng.liu@zuora.com>
2018-07-26 18:54:10 +0800

    change reference lib to my fork

--- canal/handler.go
+++ canal/handler.go
@@ -1,8 +1,8 @@
 package canal
 
 import (
+	"github.com/lishengliu/go-mysql/replication"
 	"github.com/siddontang/go-mysql/mysql"
-	"github.com/siddontang/go-mysql/replication"
 )
 
 type EventHandler interface {
